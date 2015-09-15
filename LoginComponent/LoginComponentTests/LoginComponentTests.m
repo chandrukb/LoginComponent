689a38bd3bcd58ad9fa6eb5adb7c090933671c69
Candor Apps <candoradmin@Candors-Mac-mini.local>
2015-09-15 16:59:50 +0530

    fresh commit of new project

---
+++ LoginComponent/LoginComponentTests/LoginComponentTests.m
@@ -0,0 +1,40 @@
+//
+//  LoginComponentTests.m
+//  LoginComponentTests
+//
+//  Created by Candor Apps on 15/09/15.
+//  Copyright (c) 2015 Candor Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LoginComponentTests : XCTestCase
+
+@end
+
+@implementation LoginComponentTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
